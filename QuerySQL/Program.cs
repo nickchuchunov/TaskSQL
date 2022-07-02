01d6580325d53bf861167a04280aaa76cb014b7e
nick.chuchunov <nick.chuchunov@yandex.ru>
2022-07-02 11:24:41 +0300

    SQL запрос к таблице MyProduct

---
+++ QuerySQL/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string SQL = "SELECT Categories, Produts FROM MyProducts WHERE EXIST (SELECT Categories)";
